dc0a370b90fb911c44a67f78ecdb8597d96509a2
DuxAlex <alexkrypto.ti@gmail.com>
2023-12-28 01:12:18 -0300

    iniciando projeto de cardapio para estabelecimento de comida

---
+++ src/test/java/com/seubarbosa/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seubarbosa.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
